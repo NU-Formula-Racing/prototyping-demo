8e8ea89f4bbd4f92ed73884b38d4bdbea424e373
ChrisUustal <christopheruustal@gmail.com>
2022-10-08 17:24:11 -0500

    added blink

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+const int led_pin = 22;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(led_pin, OUTPUT);
+
+  Serial.println("Startup Complete!");
+}
+
+void loop() {
+  Serial.println("Looping!");
+  digitalWrite(led_pin, HIGH);
+  delay(500);
+  digitalWrite(led_pin, LOW);
+  delay(500);
+}
